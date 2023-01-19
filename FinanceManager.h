5b2f038447282cd9c83c147627b13265656ef333
Sandra Slonina <sandra.slonina95@gmail.com>
2023-01-19 18:48:00 +0100

    Added class DateManager

---
+++ FinanceManager.h
@@ -0,0 +1,34 @@
+#ifndef FINANCEMANAGER_H
+#define FINANCEMANAGER_H
+
+#include "Income.h"
+#include "IncomesFileXml.h"
+#include "ExpensesFileXml.h"
+#include "DateManager.h"
+
+#include <string>
+#include <map>
+#include <windows.h>
+#include <cstdlib>
+#include <sstream>
+#include <ctime>
+#include <vector>
+#include <iostream>
+
+
+using namespace std;
+
+class FinanseManager
+{
+	
+	const int ID_LOGGED_USER;
+	
+    vector <Income> incomes;
+    vector <Expense> expenses;
+    
+    
+
+	
+	
+	
+};
